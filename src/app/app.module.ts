623975f0793880b03c3e639a58d86fc78a8b7c0c
Tomas <tomasporcaro22@gmail.com>
2020-10-11 16:31:32 -0300

    Angular seminario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { APP_ROUTING } from './app.routes';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+// Servicio
+import { ProductListService } from './services/product-list.services';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    ProductListService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
